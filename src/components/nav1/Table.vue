b211db706852b2f368ccb9de1b722b07c1165582
fishman17 <734583898@qq.com>
2018-01-25 22:58:42 +0800

    'From和User'

--- src/components/nav1/Table.vue
+++ src/components/nav1/Table.vue
@@ -1,13 +1,283 @@
 <template>
-  <div>Tabel</div>
+  <section>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline" style="">
+      <el-form-item >
+        <el-input v-model="formInline.user" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+       <el-form-item>
+        <el-button type="primary" @click="addUser">新增</el-button>
+      </el-form-item>
+    </el-form>
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        :default-sort = "{prop: 'date', order: 'descending'}"
+        highlight-current-row
+        @selection-change="handleSelectionChange"
+        >
+         <el-table-column
+            type="selection"
+            width="55">
+          </el-table-column>
+        <el-table-column
+          type="index"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="日期"
+          sortable
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          sortable
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          sortable
+          :formatter="formatter">
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row,scope)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-col :span="24" class="toolbar">
+        <el-button type="danger" @click="allDelete" :disabled="this.sels.length===0">批量删除</el-button>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="50" style="width: 300px; float:right">
+      </el-pagination>
+      </el-col>
+</section>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      sels: [], //列表选中列
+      formInline: {
+        user: "",
+        region: ""
+      },
+      filters: {
+        name: ""
+      },
+      loading: false,
+      users: [],
+
+      editFormVisible: false, //编辑界面是否显示
+      editLoading: false,
+      editFormRules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }]
+      },
+      //编辑界面数据
+      editForm: {
+        id: 0,
+        name: "",
+        sex: -1,
+        age: 0,
+        birth: "",
+        addr: ""
+      },
+
+      addFormVisible: false, //新增界面是否显示
+      addLoading: false,
+      addFormRules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }]
+      },
+      //新增界面数据
+      addForm: {
+        name: "",
+        sex: -1,
+        age: 0,
+        birth: "",
+        addr: ""
+      },
+
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ],
+      currentRow: null
+    };
+  },
+  methods: {
+    formatter(row, column) {
+      return row.address;
+    },
+    onSubmit() {},
+    addUser() {},
+    handleSelectionChange(val) {
+      this.sels = val;
+    },
+    handleEdit(index, row, scope) {
+      console.log(scope);
+      console.log(index, row);
+    },
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+    allDelete() {}
+  }
+};
 </script>
 
 <style>
+.toolbar {
+  text-align: left;
+}
+.el-table .cell {
+  text-align: left;
+}
+.demo-form-inline {
+  /* display: flex;
+  align-items: center;
+  justify-content: flex-start; */
+  padding: 10px;
+  text-align: left;
+  background-color: #f2f2f2;
+}
+.el-form-item {
+  margin-bottom: 0;
+}
 
+.el-pagination.is-background .el-pager li {
+  margin: 0;
+  border: 1px solid silver;
+}
 </style>
--- src/components/nav3/User.vue
+++ src/components/nav3/User.vue
@@ -1,13 +1,109 @@
 <template>
-  <div>User</div>
+<section>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline" style="">
+      <el-form-item >
+        <el-input v-model="formInline.user" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        :default-sort = "{prop: 'date', order: 'descending'}"
+        highlight-current-row
+        
+        >
+        <el-table-column
+          type="index"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="日期"
+          sortable
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          sortable
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          :formatter="formatter">
+        </el-table-column>
+      </el-table>
+</section>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      formInline: {
+        user: "",
+        region: ""
+      },
+    	filters: {
+					name: ''
+				},
+				loading: false,
+				users: [
+				],
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ],
+      currentRow: null
+    };
+  },
+  methods: {
+    formatter(row, column) {
+      console.log(row, column);
+      return row.address;
+    },
+    onSubmit(){
+      
+    }
+  }
+};
 </script>
 
 <style>
-
+.el-table .cell {
+  text-align: left;
+}
+.demo-form-inline{
+  /* display: flex;
+  align-items: center;
+  justify-content: flex-start; */
+  padding: 10px;
+  text-align: left;
+  background-color:  #f2f2f2;
+}
+.el-form-item{
+  margin-bottom: 0;
+}
 </style>
